df23a7caca200ba5aa664cc941718747fd43e641
valdoyk <osvaldovillalba-02@hotmail.com>
2025-01-29 18:07:58 -0600

    Creación de esquemas Pydantic para validación de datos

---
+++ app/schemas/recipe.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class RecipeBase(BaseModel):
+    title: str
+    description: str
+    ingredients: str
+    instructions: str
+    
+class RecipeCreate(RecipeBase):
+    pass 
+    
+class Recipe(RecipeBase):
+    id: int
+        
+    class Config:
+        orm_mode = True
